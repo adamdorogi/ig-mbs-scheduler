8fc7bf35720932050c7864fc12aaf12af3b57092
Adam Dorogi-Kaposi <adam.dorogi@gmail.com>
2022-04-28 14:56:00 +0200

    Update changed elements

--- ig_mbs_scheduler/drivers/mbs/constants/xpaths.py
+++ ig_mbs_scheduler/drivers/mbs/constants/xpaths.py
@@ -9,9 +9,9 @@ SCHEDULE_MINUTE_INPUT = "/html/body/div[*]/div[1]/div[1]/div/div/div/div/div[2]/
 SCHEDULE_PERIOD_INPUT = "/html/body/div[*]/div[1]/div[1]/div/div/div/div/div[2]/div[1]/div[2]/div/div[last()]/div[2]/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[1]/div[2]/div/div/div[2]/div[3]/div/input"
 SCHEDULE_SAVE_BUTTON = '//*[@id="facebook"]/body/div[*]/div[1]/div[1]/div/div/div/div/div[3]/div/div[2]/div'
 SCHEDULE_PLACEMENT_INPUTS = "/html/body/div[*]/div[2]/div/div/div/div/div/div/div[2]/div/div//div/div/div/div[1]/div/div/div[1]//div/label/div/div/div[1]/div[1]/div/div[1]/input"
-SCHEDULE_CAPTION_DIV = '//*[@id="facebook"]/body/div[*]/div[2]/div/div/div/div/div/div/div[2]/div/div[2]/div/div//div[1]/div/div[5]/div/div/div/div[1]/div[2]/div/div/div[1]/div/div/div/div'
-SCHEDULE_ADD_PHOTO_LINK = "/html/body/div[*]/div[2]/div/div/div/div/div/div/div[2]/div/div[1]/div/div/div/div/div[6]/div[1]/div"
-SCHEDULE_ADD_VIDEO_LINK = "/html/body/div[*]/div[2]/div/div/div/div/div/div/div[2]/div/div[1]/div/div/div/div/div[6]/div[2]/div"
+SCHEDULE_CAPTION_DIV = '//*[@id="facebook"]/body/div[*]/div[2]/div/div/div/div/div/div/div[2]/div/div[2]/div/div/div[1]/div/div/div[4]/div[2]/div/div/div/div[1]/div[2]/div/div/div[1]/div'
+SCHEDULE_ADD_PHOTO_LINK = '/html/body/div[*]/div[2]/div/div/div/div/div/div/div[2]/div/div[1]/div/div/div/div/div[5]/div[1]/div'
+SCHEDULE_ADD_VIDEO_LINK = '//*[@id="facebook"]/body/div[*]/div[2]/div/div/div/div/div/div/div[2]/div/div[1]/div/div/div/div/div[5]/div[2]/div'
 SCHEDULE_UPLOAD_FROM_DESKTOP = (
     "/html/body/div[*]/div[3]/div[1]/div[1]/div/div/div[1]/div[2]/div/div[1]/div/div"
 )
